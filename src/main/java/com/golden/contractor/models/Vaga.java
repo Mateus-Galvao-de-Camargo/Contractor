6b2fda9597f955f6fff970e8d84102c607fe7356
Mateus Galvão de Camargo <mateus.galvao.camargo.contato@gmail.com>
2024-07-21 01:47:53 -0300

    UPDATE dos getters e setters & FIX da @Entity Os gets e sets foram alterados para usarem o método de anotações da biblioteca lombok. A anotação @Entity que sinaliza para criar a tabela no banco de dados foi adicionada agora.

--- src/main/java/com/golden/contractor/models/Vaga.java
+++ src/main/java/com/golden/contractor/models/Vaga.java
@@ -1,5 +1,6 @@
 package com.golden.contractor.models;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 import  java.util.List;
@@ -13,63 +14,38 @@ import jakarta.persistence.OneToMany;
 
 import jakarta.validation.constraints.NotEmpty;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
 public class Vaga implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
     private long codigo;
 
     @NotEmpty
+    @Getter
+    @Setter
     private String nome;
 
     @NotEmpty
+    @Getter
+    @Setter
     private String data;
 
     @NotEmpty
+    @Getter
+    @Setter
     private String salario;
 
     @OneToMany(mappedBy = "vaga", cascade = CascadeType.REMOVE)
+    @Getter
+    @Setter
     private List<Candidato> candidatos;
 
-    public long getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(@NotEmpty long codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(@NotEmpty String nome) {
-        this.nome = nome;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public void setData(@NotEmpty String data) {
-        this.data = data;
-    }
-
-    public String getSalario() {
-        return salario;
-    }
-
-    public void setSalario(@NotEmpty String salario) {
-        this.salario = salario;
-    }
-
-    public List<Candidato> getCandidatos() {
-        return candidatos;
-    }
-
-    public void setCandidatos(List<Candidato> candidatos) {
-        this.candidatos = candidatos;
-    }
 }
